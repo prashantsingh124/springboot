ded5dda41c981c63f5640f31fb8582b4abc856ed
prashantsingh124 <prashantsingh12012@gmail.com>
2022-05-30 15:17:38 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
+public class DemoApplication {
+	
+	@RequestMapping("/")
+	@ResponseBody		
+		String hello() {
+		return "Hello World!";
+	}
+
+public static void main(String[] args) {
+	SpringApplication.run(DemoApplication.class, args);
+}
+
+}
